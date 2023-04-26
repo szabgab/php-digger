8d3e2dcdaa9ea71f3667bcdb189f29bd4c9d24e4
Gabor Szabo <gabor@szabgab.com>
2023-04-26 11:26:09 +0300

    experimenting

--- digger.php
+++ digger.php
@@ -1,3 +1,15 @@
 <?php
-   echo "Welcome to the PHP digger"
+    echo "Welcome to the PHP digger\n";
+    $url = "https://pear.php.net/feeds/latest.rss";
+    //echo "$url\n";
+    $handle = fopen($url, "rb");
+    $contents = '';
+    while (!feof($handle)) {
+      $contents .= fread($handle, 8192);
+    }
+    fclose($handle);
+    echo $contents;
+
+    // parse RSS
+
 ?>
